b1b55b7bc7f19cdd9181e475165dbfa7844d4683
Yogesh Gaikwad <119051813+yogeshatgit@users.noreply.github.com>
2023-02-04 19:41:12 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button'; 
 import {MatSelectModule} from '@angular/material/select';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
+import {MatListModule} from '@angular/material/list'; 
 
 @NgModule({
   declarations: [
@@ -24,7 +25,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     MatButtonModule,
     MatSelectModule,
     NgbModule,
-    
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
